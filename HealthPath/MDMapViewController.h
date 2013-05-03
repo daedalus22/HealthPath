a33127d1eeef348d72c42daef1246115c0ada5db
Dean Chen <dean.chen@gmail.com>
2013-05-03 10:58:02 -0700

    added custom and map views

---
+++ HealthPath/MDMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MDMapViewController.h
+//  HealthPath
+//
+//  Created by Dean Chen on 5/3/13.
+//  Copyright (c) 2013 Dean Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDMapViewController : UIViewController
+
+@end
---
+++ HealthPath/MDMapViewController.m
@@ -0,0 +1,38 @@
+//
+//  MDMapViewController.m
+//  HealthPath
+//
+//  Created by Dean Chen on 5/3/13.
+//  Copyright (c) 2013 Dean Chen. All rights reserved.
+//
+
+#import "MDMapViewController.h"
+
+@interface MDMapViewController ()
+
+@end
+
+@implementation MDMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
